3e8d58b9f470251374a3279a8ae3d38df0342b02
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-04-27 16:56:44 -0400

    removes existing jobs

--- app/jobs/schedule_checkins_job.rb
+++ app/jobs/schedule_checkins_job.rb
@@ -2,13 +2,15 @@ class ScheduleCheckinsJob < ActiveJob::Base
   queue_as :default
 
   def perform(recurser)
+  	remove_existing_jobs(recurser.email)
+
   	# find the correct time of day to ping
   	# Time.parse will default to TODAY
     group = Group.find(recurser.group_id)
     pingtime = Time.parse(group.time)
 
 
-    #checks if we're scheduling jobs for this week or next week
+    # checks if we're scheduling jobs for this week or next week
     # wday (4) == Thursday
     weekday = Time.zone.now.wday
     if weekday <= 4
@@ -27,14 +29,22 @@ class ScheduleCheckinsJob < ActiveJob::Base
 	  end
 	  
 	  
-    #schedule the correct number of jobs on correct days
+    # schedule the correct number of jobs on correct days
     (start..4).each do |w|
     	# adds days to the pingtime(value for + is in seconds)
     	dailypingtime = pingtime + (86400 * (w-start))
 
-    	#schedule a ping
-    	ZulipPingJob.delay(queue: recurser.name, run_at: dailypingtime).perform_later(recurser.zulip_email, group.room)
+    	# schedule a ping
+    	ZulipPingJob.delay(queue: recurser.email, run_at: dailypingtime).perform_later(recurser.zulip_email, group.room)
     end
-
   end
+
+  private
+  	def remove_existing_jobs(queue)
+  		jobs = Delayed::Job.where(queue: queue)
+  		jobs.each do |job|
+  			Delayed::Job.find(job.id).destroy
+  		end
+  	end
+
 end
