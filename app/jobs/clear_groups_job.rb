f28fa09808acb297c6f5f67d884fcd56bcc49081
James Porter <porterjamesj@gmail.com>
2016-06-06 17:37:44 -0400

    clear groups on Thursday

--- app/jobs/clear_groups_job.rb
+++ app/jobs/clear_groups_job.rb
@@ -4,8 +4,9 @@ class ClearGroupsJob < ActiveJob::Base
   def perform(*args)
     recursers = Recurser.all
     recursers.each do |rcer|
-    	rcer.update({:group_id => nil})
+      rcer.update({:group_id => nil})
     end
-    ClearGroupsJob.delay(run_at: Time.now.midnight + (86400*7)).perform_later
+    one_week_from_now = Time.now + 86400*7
+    ClearGroupsJob.delay(run_at: one_week_from_now).perform_later
   end
 end
--- bin/start_recurring_jobs.rb
+++ bin/start_recurring_jobs.rb
@@ -1,16 +1,18 @@
-weekday = Time.zone.now.wday
-
-if weekday <= 4
-  days_til_Friday = 5-weekday 	
-else
-	days_til_Friday = 6-weekday+6
+def date_of_next(day)
+  date  = Date.parse(day)
+  delta = date > Date.today ? 0 : 7
+  date + delta
 end
 
-job_time =  Time.now.midnight + (86400*days_til_Friday)
+def main
+  job_time = date_of_next("Thursday") + 12.hours
+  jobs = Delayed::Job.where(queue: "clear_groups")
 
+  if jobs.length == 0
+    ClearGroupsJob.delay(queue: "clear_groups", run_at: job_time).perform_later
+  end
+end
 
-jobs = Delayed::Job.where(queue: "clear_groups")
-
-if jobs.length == 0
-	ClearGroupsJob.delay(queue: "clear_groups", run_at: job_time).perform_later
+if __FILE__ == $0
+  main
 end
