68f914a4711c15cd28c914bc8dc697e3063aa6a1
James Porter <porterjamesj@gmail.com>
2016-06-06 18:18:21 -0400

    kill main per Dave's advice

--- bin/start_recurring_jobs.rb
+++ bin/start_recurring_jobs.rb
@@ -4,15 +4,9 @@ def date_of_next(day)
   date + delta
 end
 
-def main
-  job_time = date_of_next("Thursday") + 13.5.hours
-  jobs = Delayed::Job.where(queue: "clear_groups")
+job_time = date_of_next("Thursday") + 13.5.hours
+jobs = Delayed::Job.where(queue: "clear_groups")
 
-  if jobs.length == 0
-    ClearGroupsJob.delay(queue: "clear_groups", run_at: job_time).perform_later
-  end
-end
-
-if __FILE__ == $0
-  main
+if jobs.length == 0
+  ClearGroupsJob.delay(queue: "clear_groups", run_at: job_time).perform_later
 end
