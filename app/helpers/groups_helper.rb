89a0ab55c5dd85d022591918927f126ebbc27a17
James Porter <porterjamesj@gmail.com>
2016-06-09 22:28:38 -0400

    remove another thursday warning

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,8 +2,6 @@ module GroupsHelper
   def ping_success_helper(ping_success)
     if ping_success == "true"
       return "Your check-ins have been scheduled."
-    elsif Time.zone.now.thursday?
-      return "You haven't actually signed up for a group... Come back tomorrow!"
     else
       return "Something went wrong..."
     end
