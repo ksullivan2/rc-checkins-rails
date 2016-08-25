1984baf34f41fc9f542efe9e5c49eb03b154f070
David Albert <davidbalbert@gmail.com>
2016-08-25 11:29:51 -0400

    tabs to spaces in JavaScript files

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -3,29 +3,29 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(function(){
-	$(".group__topic__edit-button").click(function(){
-		var id = get_id("group__topic__edit-button", this.id)
-		console.log("edit clicked" + id)
-		var topicID = "#group__topic" + id
-		var topicChangeID = "#group__topic-change" + id
+  $(".group__topic__edit-button").click(function(){
+    var id = get_id("group__topic__edit-button", this.id)
+    console.log("edit clicked" + id)
+    var topicID = "#group__topic" + id
+    var topicChangeID = "#group__topic-change" + id
 
-		$(topicID).hide()
-		$(topicChangeID).show()
-	})
+    $(topicID).hide()
+    $(topicChangeID).show()
+  })
 
-	$(".group__topic-change__cancel-button").click(function(){
-		var id = get_id("group__topic-change__cancel-button", this.id)
-		console.log("cancel clicked" + id)
-		var topicID = "#group__topic" + id
-		var topicChangeID = "#group__topic-change" + id
+  $(".group__topic-change__cancel-button").click(function(){
+    var id = get_id("group__topic-change__cancel-button", this.id)
+    console.log("cancel clicked" + id)
+    var topicID = "#group__topic" + id
+    var topicChangeID = "#group__topic-change" + id
 
-		$(topicID).show()
-		$(topicChangeID).hide()
-	})
+    $(topicID).show()
+    $(topicChangeID).hide()
+  })
 })
 
 
 function get_id(partial, full){
-	return full.substring(partial.length)
+  return full.substring(partial.length)
 }
 
