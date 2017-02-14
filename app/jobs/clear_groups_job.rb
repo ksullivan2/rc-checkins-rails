0b3d217c0de7540e0a5d52b0a35f844eaee32061
James Porter <james@jamesporter.me>
2017-02-14 15:45:58 -0500

    clear groups also

--- app/jobs/clear_groups_job.rb
+++ app/jobs/clear_groups_job.rb
@@ -6,6 +6,10 @@ class ClearGroupsJob < ActiveJob::Base
     recursers.each do |rcer|
       rcer.update({:group_id => nil})
     end
+    groups = Group.all
+    groups.each do |group|
+      group.update({:topic => "Anything!"})
+    end
     ClearGroupsJob.delay(run_at: 1.week.from_now).perform_later
   end
 end
